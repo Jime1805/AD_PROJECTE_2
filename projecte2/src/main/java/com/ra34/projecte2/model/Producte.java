2a13d59c96fb8927116f566e4fc19895313d0a97
Jime1805 <18ejimen@gmail.com>
2026-02-25 19:59:42 +0100

    models

--- projecte2/src/main/java/com/ra34/projecte2/model/Producte.java
+++ projecte2/src/main/java/com/ra34/projecte2/model/Producte.java
@@ -1,5 +1,29 @@
 package com.ra34.projecte2.model;
 
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity(name="productes")
 public class Producte {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String descripcion;
+    private int stock;
+    private float price;
+    private float rating;
+    private String condition;
+
+    @Enumerated(value = EnumType.STRING)
+    
+
+
 
 }
