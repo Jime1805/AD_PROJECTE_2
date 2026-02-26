f79b8d6d9e73b476b9aa9732513f7445f68901f2
Jime1805 <18ejimen@gmail.com>
2026-02-26 10:32:17 +0100

    model acabat

--- projecte2/src/main/java/com/ra34/projecte2/model/Producte.java
+++ projecte2/src/main/java/com/ra34/projecte2/model/Producte.java
@@ -1,6 +1,9 @@
 package com.ra34.projecte2.model;
 
+import java.util.Date;
+
 import org.springframework.data.annotation.Id;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -19,11 +22,112 @@ public class Producte {
     private int stock;
     private float price;
     private float rating;
-    private String condition;
 
     @Enumerated(value = EnumType.STRING)
+    private Condition condition;
     
+    private boolean status;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date dataCreated;
+    
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date dateUpDate;
 
+    public Producte() {
+    }
 
+    public Producte(Long id, String nombre, String descripcion, int stock, float price, float rating,
+            Condition condition, boolean status, Date dataCreated, Date dateUpDate) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.stock = stock;
+        this.price = price;
+        this.rating = rating;
+        this.condition = condition;
+        this.status = status;
+        this.dataCreated = dataCreated;
+        this.dateUpDate = dateUpDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    public Condition getCondition() {
+        return condition;
+    }
+
+    public void setCondition(Condition condition) {
+        this.condition = condition;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public Date getDataCreated() {
+        return dataCreated;
+    }
+
+    public void setDataCreated(Date dataCreated) {
+        this.dataCreated = dataCreated;
+    }
+
+    public Date getDateUpDate() {
+        return dateUpDate;
+    }
+
+    public void setDateUpDate(Date dateUpDate) {
+        this.dateUpDate = dateUpDate;
+    }
 }
