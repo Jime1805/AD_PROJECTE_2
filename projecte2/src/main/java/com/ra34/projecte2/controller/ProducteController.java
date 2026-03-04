0d73ce8c35f69c7c3ccc7ce6c17b342e5181b45f
ldloler <marc.be.pi@gmail.com>
2026-03-04 19:30:31 +0100

    Afegit anotacions principals

--- projecte2/src/main/java/com/ra34/projecte2/controller/ProducteController.java
+++ projecte2/src/main/java/com/ra34/projecte2/controller/ProducteController.java
@@ -1,6 +1,12 @@
 package com.ra34.projecte2.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class ProducteController {
+
 
 
     // Separació Eric a baix, Marc a dalt.
