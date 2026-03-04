6b295c4b6b13e53e641e3d2d92a5adc7d3448fd5
ldloler <marc.be.pi@gmail.com>
2026-03-04 19:57:10 +0100

    Afegit endpoints updateEstoc i updatePreu. Falta comprobar.

--- projecte2/src/main/java/com/ra34/projecte2/controller/ProducteController.java
+++ projecte2/src/main/java/com/ra34/projecte2/controller/ProducteController.java
@@ -1,13 +1,37 @@
 package com.ra34.projecte2.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ra34.projecte2.service.ProducteService;
+import org.springframework.web.bind.annotation.PatchMapping;
+
+
 
 @RestController
 @RequestMapping("/api")
 public class ProducteController {
 
+    @Autowired
+    ProducteService producteService;
 
+    
+    @PatchMapping("/producte/estoc")
+    public ResponseEntity<String> updateEstocProducte(@RequestParam Long id, @RequestParam int newEstoc) {
+        String updated = producteService.updateEstocProducte(id, newEstoc);
+        return ResponseEntity.status(HttpStatus.OK).body(updated);
+    }
+
+    @PatchMapping("/producte/preu")
+    public ResponseEntity<String> updatePreuProducte(@RequestParam Long id, @RequestParam int newPreu) {
+        String updated = producteService.updatePreuProducte(id, newPreu);
+        return ResponseEntity.status(HttpStatus.OK).body(updated);
+    }
+    
 
     // Separació Eric a baix, Marc a dalt.
 
