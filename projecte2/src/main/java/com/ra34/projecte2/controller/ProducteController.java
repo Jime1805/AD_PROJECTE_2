7a284941b59e946d2545f985680b4b78cf404530
ldloler <marc.be.pi@gmail.com>
2026-03-04 19:23:57 +0100

    Afegit separacions

--- projecte2/src/main/java/com/ra34/projecte2/controller/ProducteController.java
+++ projecte2/src/main/java/com/ra34/projecte2/controller/ProducteController.java
@@ -2,4 +2,7 @@ package com.ra34.projecte2.controller;
 
 public class ProducteController {
 
+
+    // Separació Eric a baix, Marc a dalt.
+
 }
--- projecte2/src/main/java/com/ra34/projecte2/service/ProducteService.java
+++ projecte2/src/main/java/com/ra34/projecte2/service/ProducteService.java
@@ -2,4 +2,7 @@ package com.ra34.projecte2.service;
 
 public class ProducteService {
 
+
+    // Separació Eric a baix, Marc a dalt.
+
 }
