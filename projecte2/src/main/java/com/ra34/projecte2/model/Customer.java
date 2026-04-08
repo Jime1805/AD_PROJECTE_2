108166ab97a051c18ffeb82b473ac9d384086a21
ldloler <marc.be.pi@gmail.com>
2026-04-08 19:12:42 +0200

    Customer creat

---
+++ projecte2/src/main/java/com/ra34/projecte2/model/Customer.java
@@ -0,0 +1,108 @@
+package com.ra34.projecte2.model;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private Long userId;
+    private String firstName;
+    private String lastName;
+    private String phone;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    private List<Address> addresses;
+    private boolean status;
+    private Timestamp dataCreated;
+    private Timestamp dataUpdated;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public Timestamp getDataCreated() {
+        return dataCreated;
+    }
+
+    public void setDataCreated(Timestamp dataCreated) {
+        this.dataCreated = dataCreated;
+    }
+
+    public Timestamp getDataUpdated() {
+        return dataUpdated;
+    }
+
+    public void setDataUpdated(Timestamp dataUpdated) {
+        this.dataUpdated = dataUpdated;
+    }
+
+    public Customer(Long id, Long userId, String firstName, String lastName, String phone) {
+        this.id = id;
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+    }
+
+}
