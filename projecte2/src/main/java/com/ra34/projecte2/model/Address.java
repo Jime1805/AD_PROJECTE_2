a66f73ec8ae3cfffd68b9df32638e3bfaee732de
ldloler <marc.be.pi@gmail.com>
2026-04-08 19:46:09 +0200

    Taules logiques d'usuari

---
+++ projecte2/src/main/java/com/ra34/projecte2/model/Address.java
@@ -0,0 +1,92 @@
+package com.ra34.projecte2.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "address")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Customer customer;
+    private String address;
+    private String city;
+    private String postalCode;
+    private String country;
+    private boolean isDefault;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public boolean isDefault() {
+        return isDefault;
+    }
+
+    public void setDefault(boolean isDefault) {
+        this.isDefault = isDefault;
+    }
+
+    public Address(Long id, Customer customer, String address, String city, String postalCode, String country,
+            boolean isDefault) {
+        this.id = id;
+        this.customer = customer;
+        this.address = address;
+        this.city = city;
+        this.postalCode = postalCode;
+        this.country = country;
+        this.isDefault = isDefault;
+    }
+
+}
---
+++ projecte2/src/main/java/com/ra34/projecte2/model/User.java
@@ -0,0 +1,81 @@
+package com.ra34.projecte2.model;
+
+import java.sql.Timestamp;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    private boolean status;
+    private Timestamp dataCreated;
+    private Timestamp dataUpdated;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public Timestamp getDataCreated() {
+        return dataCreated;
+    }
+
+    public void setDataCreated(Timestamp dataCreated) {
+        this.dataCreated = dataCreated;
+    }
+
+    public Timestamp getDataUpdated() {
+        return dataUpdated;
+    }
+
+    public void setDataUpdated(Timestamp dataUpdated) {
+        this.dataUpdated = dataUpdated;
+    }
+
+    public User(Long id, Customer customer, String email, String password) {
+        this.id = id;
+        this.customer = customer;
+        this.email = email;
+        this.password = password;
+    }
+
+}
