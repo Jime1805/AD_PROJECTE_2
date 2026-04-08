fe69a56ab8c850f8e807ca933efce6267a7a1e36
ldloler <marc.be.pi@gmail.com>
2026-04-08 19:47:31 +0200

    Added setters, getters i constructor

--- projecte2/src/main/java/com/ra34/projecte2/model/Role.java
+++ projecte2/src/main/java/com/ra34/projecte2/model/Role.java
@@ -23,4 +23,34 @@ public class Role {
 
     @ManyToMany(mappedBy = "user")
     private List<User> users = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public RoleName getName() {
+        return name;
+    }
+
+    public void setName(RoleName name) {
+        this.name = name;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public Role(Long id, RoleName name, List<User> users) {
+        this.id = id;
+        this.name = name;
+        this.users = users;
+    }
 }
