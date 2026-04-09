36e39d43761c6189e61a2dab2b332a9040e5df6a
Jime1805 <18ejimen@gmail.com>
2026-04-09 09:17:03 +0200

    ara esta igual que a la branca eric

---
+++ projecte2/src/main/java/com/ra34/projecte2/model/OrderItem.java
@@ -0,0 +1,27 @@
+package com.ra34.projecte2.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "order_item")
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "orderId", unique = true)
+    private Order orderId;
+
+    // productId cuando haga model de Product
+
+    private int quantity;
+    private double untilPrice;
+}
