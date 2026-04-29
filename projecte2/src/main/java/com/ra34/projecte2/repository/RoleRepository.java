862a6eb7113046c976a32bcce28f8231ccd7ad61
Jime1805 <18ejimen@gmail.com>
2026-04-29 18:26:14 +0200

    compila

---
+++ projecte2/src/main/java/com/ra34/projecte2/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.ra34.projecte2.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ra34.projecte2.model.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
